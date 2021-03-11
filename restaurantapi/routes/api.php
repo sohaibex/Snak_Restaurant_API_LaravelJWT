2ec32573daab42ec28dc2dd4f9b0487180b3e9e5
SOHAIB EL MEDIOUNI <sohaib.elmediouni23@gmail.com>
2021-03-11 22:50:29 +0100

    auth

--- restaurantapi/routes/api.php
+++ restaurantapi/routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('login', 'ApiController@login');
+Route::post('register', 'ApiController@register');
+
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'ApiController@logout');
+
+    Route::resource('commandes', 'CommandeController');
 });
